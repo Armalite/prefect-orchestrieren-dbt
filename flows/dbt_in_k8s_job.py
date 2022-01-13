80e8d121da3c84d0426caed3115127bf58e6527a
armalite <ttc_rulz@hotmail.com>
2022-01-14 10:52:04 +1300

    yaml import, to convert k8s job template to dict before passing it to prefect task

--- flows/dbt_in_k8s_job.py
+++ flows/dbt_in_k8s_job.py
@@ -2,16 +2,19 @@ import prefect
 from prefect import task, Flow
 from prefect.tasks.kubernetes import RunNamespacedJob
 from prefect.run_configs import LocalRun, KubernetesRun, RunConfig
+import yaml
 
 job_template = open('kube/test_cronjob.yaml')
+job_template_dict = yaml.load(job_template)
 
-job_run_task = RunNamespacedJob(body=job_template)
+k8s_job_run_task = RunNamespacedJob(body=job_template_dict)
 
 @task
 def k8s_hello_task():
     logger = prefect.context.get("logger")
     logger.info("Hello, Beautiful Kubernetes!")
 
-flow = Flow("k8s-greetings", run_config=KubernetesRun(), tasks=[k8s_hello_task])
+flow = Flow("k8s-greetings", run_config=KubernetesRun(), tasks=[k8s_hello_task,k8s_job_run_task])
 
-flow.register(project_name="beautiful-test")
+flow.run()
+#flow.register(project_name="beautiful-test")
