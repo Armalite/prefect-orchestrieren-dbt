85a4163f210f516b5208b044e43d27a9dcaf0c6b
armalite <ttc_rulz@hotmail.com>
2022-01-14 10:28:55 +1300

    Basic/skeleton flows to run in k8s

--- flows/dbt_in_k8s_job.py
+++ flows/dbt_in_k8s_job.py
@@ -1,7 +1,17 @@
+import prefect
+from prefect import task, Flow
 from prefect.tasks.kubernetes import RunNamespacedJob
+from prefect.run_configs import LocalRun, KubernetesRun, RunConfig
 
-job_template = open('kube/dbt_cronjob.yaml')
+job_template = open('kube/test_cronjob.yaml')
 
 job_run_task = RunNamespacedJob(body=job_template)
 
-print("Hello lol")
+@task
+def k8s_hello_task():
+    logger = prefect.context.get("logger")
+    logger.info("Hello, Beautiful Kubernetes!")
+
+flow = Flow("k8s-greetings", run_config=KubernetesRun(), tasks=[k8s_hello_task])
+
+flow.register(project_name="beautiful-test")
